1488984ba538cb91d080666c6dae0d84ebf62888
Denis <denis@The-Machine.local>
2023-08-06 05:29:30 -0700

    Add linter

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+
 import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
@@ -69,7 +70,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
     }
-} 
+}
 
 
 # Password validation
